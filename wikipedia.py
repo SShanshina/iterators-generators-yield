73a1e385ae139b01ed839254f936b7eac6c73813
Sofia Shanshina <s.shanshina@gmail.com>
2020-12-26 15:38:15 +0700

    Files modified

--- wikipedia.py
+++ wikipedia.py
@@ -3,41 +3,43 @@ import json
 from tqdm import tqdm
 
 
-class WikiIter:
+class CountryLinks:
 
-    def __init__(self, country_name):
-        self.country_name = country_name
-        self.response = requests.get(
-            'http://wikipedia.org/w/api.php',
-            params={
-                'action': 'opensearch',
-                'search': {self.country_name},
-                'inprop': 'url'
-            })
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.counter = -1
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        while self.country_name:
-            url = self.response.json()[3]
-            return url
+
+        with open(self.file_name) as file:
+            data = json.load(file)
+
+        self.counter += 1
+
+        while self.counter < len(data):
+            country = data[self.counter]['name']['official']
+            response = requests.get(
+                        'http://wikipedia.org/w/api.php',
+                        params={
+                            'action': 'opensearch',
+                            'search': {country},
+                            'inprop': 'url'
+                        })
+            url_list = response.json()[3]
+            if len(url_list) > 0:
+                url = url_list[0]
+            else:
+                url = 'This page does not exist'
+            return f'{country}: {url}'
+
         else:
             raise StopIteration
 
 
 if __name__ == '__main__':
-    with open('countries.json') as file:
-        data = json.load(file)
-    countries = []
-    for country in data:
-        country_name = country['name']['official']
-        countries.append(country_name)
     with open('countries.txt', 'w', encoding='utf-8') as f:
-        for country in tqdm(countries):
-            wiki_url = WikiIter(country)
-            if len(next(wiki_url)) > 0:
-                url = next(wiki_url)[0]
-                f.write(f'{country}: {url}\n')
-            else:
-                f.write(f'{country}: This page does not exist\n')
+        for country_link in tqdm(CountryLinks('countries.json')):
+            f.write(f'{country_link}\n')
